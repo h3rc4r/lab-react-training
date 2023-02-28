90397e2f528681e18d7504ba4ebc42f075544276
Hernando Cardenas <h3rc4rm@gmail.com>
2023-02-28 14:53:48 +0100

    wip

--- src/components/CreditCard.js
+++ src/components/CreditCard.js
@@ -5,8 +5,8 @@ const CreditCard = ({...props}) => {
   return (
     <div className='moneyCard' style={estilos}>
         <div className='right'>
-            <img width={45} src= {type === "Visa" ? "https://logodownload.org/wp-content/uploads/2016/10/visa-logo-1.png"
-                : "https://logodownload.org/wp-content/uploads/2014/07/mastercard-logo.png"} alt=""
+            <img width={45} src= {type === "Visa" ? "https://1000marcas.net/wp-content/uploads/2019/12/VISA-Logo.png"
+                : "https://1361832452.rsc.cdn77.org/gesa/files/2560px-MasterCard_Logo.svg_.png?1626945991"} alt=""
             />
         </div>
         <div className='numberCard'><strong><p>**** **** **** {number.toString().slice(-4)}</p></strong></div>
